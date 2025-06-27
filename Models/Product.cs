d4df32969b3cf4f7e9c4c1bc09816bdf2c04275b
hoanghuanbee <huan12345ndnd@gmail.com>
2025-06-27 12:40:09 +0700

    Thêm nội dung cho model Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,7 +1,17 @@
-﻿namespace BlueElectronic.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlueElectronic.Models
 {
     public class Product
     {
-        
+        public int Id { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int ProductPrice { get; set; }
+        public string Img1 { get; set; }
     }
 }
